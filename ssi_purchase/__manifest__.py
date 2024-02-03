683b75fcbe0a33aa2f5a4b00e4278d9098fcd659
Andhitia Rama <andhitia.r@gmail.com>
2024-02-03 11:50:00 +0700

    [UPD] ssi_purchase
    * Change license into AGPL

--- ssi_purchase/__manifest__.py
+++ ssi_purchase/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2023 OpenSynergy Indonesia
 # Copyright 2023 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase",
     "version": "14.0.2.0.0",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
     "application": True,
     "depends": [
--- ssi_purchase/models/purchase_order_type.py
+++ ssi_purchase/models/purchase_order_type.py
@@ -1,15 +1,14 @@
 # Copyright 2023 OpenSynergy Indonesia
 # Copyright 2023 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
+from odoo import models
 
 
 class PurchaseOrderType(models.Model):
     _name = "purchase_order_type"
     _inherit = [
-        'mixin.master_data',
+        "mixin.master_data",
     ]
     _description = "Purchase Order Type"
     _field_name_string = "Purchase Order Type"
